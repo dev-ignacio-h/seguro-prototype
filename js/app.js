5596dd6b30fb335207645c1ec9c98e14489ece70
Ignacio <dev.ignacio.h@gmail.com>
2021-01-29 23:45:22 -0300

    validaciones

--- js/app.js
+++ js/app.js
@@ -28,3 +28,25 @@ console.log(ui);
 document.addEventListener("DOMContentLoaded", () => {
     ui.llenarOpciones(); //llena el select con los años...
 });
+
+eventListeners();
+
+function eventListeners() {
+    const formulario = document.querySelector("#cotizar-seguro");
+    formulario.addEventListener("submit", cotizarSeguro);
+}
+
+function cotizarSeguro(e) {
+    e.preventDefault();
+    // Leer la marca seleccionada
+    const marca = document.querySelector("#marca").value;
+
+    // Leer el año seleccionado
+    const year = document.querySelector("#year").value;
+
+    // Leer el tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+    if (marca === '' || year === '' || tipo === '') console.log('no pasó la validación');
+    else console.log('Sí pasó');
+}
