73b0aa68e985380024f751690423bfecb1743675
Ignacio <dev.ignacio.h@gmail.com>
2021-01-30 00:08:12 -0300

    mensaje de error y extito

--- js/app.js
+++ js/app.js
@@ -21,6 +21,27 @@ UI.prototype.llenarOpciones = () => {
     }
 };
 
+// muestra alertas en pantalla
+UI.prototype.mostarMensaje = function(mensaje, tipo) {
+    const div = document.createElement('div');
+    if (tipo === 'error') {
+        div.classList.add('error');
+    } else {
+        div.classList.add('correcto');
+    }
+
+    div.classList.add('mensaje', 'mt-10');
+    div.textContent = mensaje;
+
+    // insertar en el HTML
+    const formulario = document.querySelector("#cotizar-seguro");
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+
+    setTimeout(() => {
+        div.remove()
+    }, 3000);
+}
+
 // instanciar UI
 const ui = new UI();
 console.log(ui);
@@ -47,6 +68,13 @@ function cotizarSeguro(e) {
     // Leer el tipo de cobertura
     const tipo = document.querySelector('input[name="tipo"]:checked').value;
 
-    if (marca === '' || year === '' || tipo === '') console.log('no pasó la validación');
-    else console.log('Sí pasó');
+    if (marca === '' || year === '' || tipo === '') {
+        ui.mostarMensaje('Todos los campos son obligatorios', 'error');
+        return
+    }
+    ui.mostarMensaje('Cotizando...');
+
+    // Instanciar el seguro
+
+    // Utilizar el prototype que va a cotizar
 }
